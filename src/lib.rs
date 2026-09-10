c9854f9393ba8f53ec7e3d9706362abdd34f0e5f
Suraj Kumar <suraj410401@gmail.com>
2026-09-10 07:03:34 +0530

    Add crate manifest and crate root

---
+++ src/lib.rs
@@ -0,0 +1,9 @@
+//! UCAN 1.0 delegation, invocation and proof chain validation, with
+//! classical and post-quantum `did:key` principals in the same chain.
+
+#![cfg_attr(not(feature = "std"), no_std)]
+#![forbid(unsafe_code)]
+
+extern crate alloc;
+
+pub use ipld_core::{cid::Cid, ipld::Ipld};
